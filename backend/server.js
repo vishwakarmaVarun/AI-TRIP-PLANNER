a9792319ad4d51ad1b6d06c882066516164a5544
varun vishwakarma <varun@webwithvarun.com>
2024-08-21 12:58:07 +0530

    some changes

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,36 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
-import cors from 'cors'
-import authRouter from './routes/auth.route.js'
+import cors from "cors";
+import authRouter from "./routes/auth.route.js";
 import { connectDB } from "./db/db.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
-app.use(cors())
-app.use(express.json())
-app.use(cookieParser())
 
-app.use('/api/auth', authRouter)
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.use("/api/auth", authRouter);
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+});
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    })
-})
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 connectDB()
   .then(() => {
